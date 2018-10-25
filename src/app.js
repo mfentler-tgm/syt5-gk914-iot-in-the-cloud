ee19875f476f5f802f8e02503cdd9811e891eca2
Christoph Pader-Barosch <christoph.paderbarosch@gmail.com>
2018-10-25 11:17:59 +0200

    Updated default app to email

---
+++ src/app.js
@@ -0,0 +1,27 @@
+var nodemailer = require("nodemailer")
+
+var transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: "shittingPreventionAgent@gmail.com",
+    pass: "NoShittingAnymore"
+  },
+  tls: {
+    rejectUnauthorized: false
+  }
+})
+
+var mailOptions = {
+  from: "shittingPreventionAgent@gmail.com",
+  to: ["michaelborko@gmail.com", "chrispad2k@gmail.com"],
+  subject: "Email von unserem Raspi",
+  text: "That was easy!"
+}
+
+transporter.sendMail(mailOptions, function(error, info) {
+  if (error) {
+    console.log(error)
+  } else {
+    console.log("Email sent: " + info.response)
+  }
+})
