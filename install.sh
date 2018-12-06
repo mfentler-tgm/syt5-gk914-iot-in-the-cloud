718737274f694f89ffc7ef4c731477fe57b29d1f
Christoph Pader-Barosch <christoph.paderbarosch@gmail.com>
2018-12-06 16:04:57 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ apt update -y
 apt upgrade -y
 apt dist-upgrade -y
 
-apt install -y python2.7 python-pip
+apt install -y python2.7 python-pip python3 python3-pip
 apt install -y vi vim
 apt install -y git
 apt install -y nodejs npm
