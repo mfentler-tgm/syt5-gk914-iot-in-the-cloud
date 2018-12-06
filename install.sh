2eb281f2461160ee93a344adad6ef29624d4c81e
Christoph Pader-Barosch <christoph.paderbarosch@gmail.com>
2018-12-06 14:22:56 +0100

    added install script

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+apt update -y
+apt upgrade -y
+apt dist-upgrade -y
+
+apt install -y python2.7 python-pip
+apt install -y vi vim
+apt install -y git
+apt install -y nodejs npm
+
+npm config set python python2.7
+
+git clone https://github.com/TGM-HIT/syt5-gk914-iot-in-the-cloud-basement-shitting-prevention-team /app
+cd app
+npm install
