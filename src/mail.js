4e704ef838895facfaf00e0edf8c057ce9fefda3
Christoph Pader-Barosch <christoph.paderbarosch@gmail.com>
2018-12-06 10:36:25 +0100

    added docker

--- src/mail.js
+++ src/mail.js
@@ -2,33 +2,32 @@ import * as ip from 'ip'
 import * as nodemailer from 'nodemailer'
 
 const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-        user: 'shittingPreventionAgent@gmail.com',
-        pass: 'NoShittingAnymore'
-    },
-    tls: {
-        rejectUnauthorized: false
-    }
+  service: 'gmail',
+  auth: {
+    user: 'shittingPreventionAgent@gmail.com',
+    pass: 'NoShittingAnymore'
+  },
+  tls: {
+    rejectUnauthorized: false
+  }
 })
 
-export const sendMail = function () {
+export const sendMail = function() {
+  const mailOptions = {
+    from: 'shittingPreventionAgent@gmail.com',
+    to: ['chrispad2k@gmail.com', 'marcrousavy@hotmail.com'],
+    subject: '[Shitting Agent] Intruder detected',
+    text: `Someone is trying to fill your box again.. </br></br>At: ${Date()} </br></br>IP: ${ip.address()}`
+  }
 
-    const mailOptions = {
-        from: 'shittingPreventionAgent@gmail.com',
-        to: ['chrispad2k@gmail.com', 'marcrousavy@hotmail.com'],
-        subject: '[Shitting Agent] Intruder detected',
-        text: `Someone is trying to fill your box again.. </br></br>At: ${Date()} </br></br>IP: ${ip.address()}`
+  console.log('Sending Email ...')
+
+  transporter.sendMail(mailOptions, function(error, info) {
+    if (error) {
+      console.log(error)
+    } else {
+      console.log('Email sent: ' + info.response)
     }
-
-    console.log('Sending Email ...')
-
-    transporter.sendMail(mailOptions, function (error, info) {
-        if (error) {
-            console.log(error)
-        } else {
-            console.log('Email sent: ' + info.response)
-        }
-    })
+  })
 
 }
