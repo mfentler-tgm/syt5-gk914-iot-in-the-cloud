bc6de6121d904b73aaac7b4543c1a36f34148ec7
Christoph Pader-Barosch <christoph.paderbarosch@gmail.com>
2018-12-06 14:29:25 +0100

    simplified webserver

--- src/app.js
+++ src/app.js
@@ -1,8 +1,11 @@
 // var Gpio = require('onoff').Gpio,
 // pir = new Gpio(17, 'in', 'both');
 var mail = require('./mail')
+var webserver = require('./webserver')
 
 mail.sendMail()
+
+webserver.startServer()
 
 /**
 pir.watch(function(err, value) {
--- src/webserver.js
+++ src/webserver.js
@@ -1,7 +1,4 @@
-import * as fs from 'fs'
-import * as http from 'http'
-import * as path from 'path'
-import * as urlLib from 'url'
+const http = require('http')
 const PiCamera = require('pi-camera')
 
 const PORT = 8080
@@ -16,93 +13,23 @@ const myCamera = new PiCamera({
   nopreview: true
 })
 
-export const startServer = function() {
-  const server = http.createServer((req, res) => {
-    const {url} = req
-    const pwd = process.cwd()
-    const filePath = path.join(pwd, url)
-
-    myCamera.snap()
-      .then((result) => {
-        res.end(`<html><head></head><body><img src="${imgpath}"/></body></html>`)
-        return
-      })
-      .catch((error) => {
-        // Handle your error
-      })
-
-    fs.exists(filePath, (exists) => {
-      const responseTypes = {
-        html: {'Content-Type': 'text/html'},
-        plain: {'Content-Type': 'text/plain'}
-      }
-
-      res.writeHead(200, responseTypes.html)
-
-      if (exists) {
-        let data = `<div>${filePath}</div>`
-
-        fs.lstat(filePath, (err, stats) => {
-          if (err) {
-            res.writeHead(200, responseTypes.plain)
-            res.end(err.message)
-          }
-
-          if (stats.isDirectory()) {
-            fs.readdir(filePath, (errIsDir, files) => {
-              if (errIsDir) {
-                res.writeHead(200, responseTypes.plain)
-                res.end(err.message)
-              }
-
-              if (files.length === 0) {
-                data += '<div>There are no files in this directory</div>'
-              }
-
-              let fileString = ''
-
-              files.map((file) => {
-                fileString +=
-                  `<li><a href="${urlLib.resolve(`http://localhost:${PORT}`, path.join(url, file))}">${file}</a></li>`
-              })
-
-              data += `
-                <ul>
-                ${fileString}
-                </ul>
-                <a href="http://localhost:${PORT}">Home</a>
-              `
-
-              res.end(data)
-            })
-          } else if (stats.isFile()) {
-            fs.readFile(filePath, (errIsFile, content) => {
-              if (errIsFile) {
-                res.writeHead(200, responseTypes.plain)
-                res.end(err.message)
-              }
-
-              data += `
-                <p>
-                ${content.toString().replace(/(?:\r\n|\r|\n)/g, '<br>')}
-                </p>
-                <a href="http://localhost:${PORT}">Home</a>
-              `
-
-              res.end(data)
-            })
-          }
+module.exports = {
+  startServer: function() {
+    const server = http.createServer((req, res) => {
+      myCamera.snap()
+        .then((result) => {
+          res.end(`<html><head></head><body><img src="${imgpath}"/></body></html>`)
+          return
         })
-      } else {
-        res.writeHead(404, responseTypes.plain)
-        res.end('File or directory not found')
-      }
+        .catch((error) => {
+          res.end(`Error! ${error}`)
+        })
+    }).listen(PORT)
+
+    server.on('close', () => {
+      console.log('Server shut down')
     })
-  }).listen(PORT)
 
-  server.on('close', () => {
-    console.log('Server shut down')
-  })
-
-  console.log(`Server running on localhost:${PORT}`)
+    console.log(`Server running on localhost:${PORT}`)
+  }
 }
