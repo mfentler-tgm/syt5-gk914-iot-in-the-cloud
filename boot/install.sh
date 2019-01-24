0ab9db56a525b913beefb1fa36df08e94864a96f
Christoph Pader-Barosch <christoph.paderbarosch@gmail.com>
2019-01-24 09:15:30 +0100

    Implemented Livestream with raspi-live, hls.js and express

--- boot/install.sh
+++ boot/install.sh
@@ -8,10 +8,20 @@ apt install -y python2.7 python-pip python3 python3-pip
 apt install -y vi vim
 apt install -y git
 apt install -y nodejs npm
+apt install libomxil-bellagio-dev
+
+wget -O ffmpeg.tar.bz2 https://ffmpeg.org/releases/ffmpeg-snapshot-git.tar.bz2
+tar xvjf ffmpeg.tar.bz2
+cd ffmpeg
+sudo ./configure --arch=armel --target-os=linux --enable-gpl --enable-omx --enable-omx-rpi --enable-nonfree
 
 npm config set python python2.7
 
+npm install -g raspi-live
+
+rasp-live start -p 3000 &
+
 git clone https://github.com/TGM-HIT/syt5-gk914-iot-in-the-cloud-basement-shitting-prevention-team /app
-cd app
+cd /app
 npm install
 npm start
--- src/webserver.js
+++ src/webserver.js
@@ -1,42 +1,51 @@
-const http = require('http')
 const PiCamera = require('pi-camera')
 const ip = require('ip')
-const fs = require('fs')
-const url = require('url')
-
+const path = require('path')
+const express = require('express')
 
 const PORT = 80
+const ADDRESS = ip.address()
 
-var imgpath = `${__dirname}/test.jpg`
-var address = ip.address()
+// var imgpath = `${__dirname}/test.jpg`
 
-const myCamera = new PiCamera({
-  mode: 'photo',
-  output: imgpath,
-  width: 640,
-  height: 480,
-  nopreview: true
+// const myCamera = new PiCamera({
+//   mode: 'photo',
+//   output: imgpath,
+//   width: 640,
+//   height: 480,
+//   nopreview: true
+// })
+
+const app = express()
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'))
 })
 
 module.exports = {
+  // startServer: function() {
+  //   const server = http.createServer((req, res) => {
+  //     myCamera.snap()
+  //       .then((result) => {
+  //         var img = fs.readFileSync(imgpath)
+  //         res.writeHead(200, {'Content-Type': 'image/jpg'})
+  //         res.end(img, 'binary')
+  //         return
+  //       })
+  //       .catch((error) => {
+  //         res.end(`Error! ${error}`)
+  //       })
+  //   }).listen(PORT)
+
+  //   server.on('close', () => {
+  //     console.log('Server shut down')
+  //   })
+
+  //   console.log(`Server running on localhost:${PORT}`)
+  // }
   startServer: function() {
-    const server = http.createServer((req, res) => {
-      myCamera.snap()
-        .then((result) => {
-          var img = fs.readFileSync(imgpath)
-          res.writeHead(200, {'Content-Type': 'image/jpg'})
-          res.end(img, 'binary')
-          return
-        })
-        .catch((error) => {
-          res.end(`Error! ${error}`)
-        })
-    }).listen(PORT)
-
-    server.on('close', () => {
-      console.log('Server shut down')
+    app.listen(PORT, () => {
+      console.log(`Webserver with Livestream listening on ${ADDRESS}:${PORT}`)
     })
-
-    console.log(`Server running on localhost:${PORT}`)
   }
 }
