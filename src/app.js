50928affb4c37948dedf0e680611c9d814c1add6
Mario Fentler <mfentler@student.tgm.ac.at>
2019-02-14 14:26:57 +0100

    Added sqlite3 db

--- src/app.js
+++ src/app.js
@@ -4,6 +4,8 @@ const Gpio = require('onoff').Gpio
 
 const pir = new Gpio(17, 'in', 'both')
 
+const sqlite = require('sqlite3')
+
 var mailEnabled = false
 
 process.argv.slice(1).map((argument) => {
@@ -25,6 +27,26 @@ pir.watch(function(err, value) {
 
   if (value == 1) {
     mail.sendMail()
+
+    //open db connection
+    let db = new sqlite.Database('./db/sensorDaten.db', (err) => {
+      if (err) {
+        return console.error(err.message)
+      }
+      console.log('Connected to the in-memory SQlite database.')
+    })
+
+    db.run("CREATE TABLE IF NOT EXISTS aufzeichnung (id INT primary key, timestamp DATE, bild TEXT);")
+
+    var timestamp = new Date().getTime()
+
+    //Close connection again
+    db.close((err) => {
+      if (err) {
+        return console.error(err.message)
+      }
+      console.log('Close the database connection.')
+    })
   }
 })
 
